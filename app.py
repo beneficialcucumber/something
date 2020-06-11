868f8a1dc31f93e5074bb0007a33b3df1867eead
Your Name <you@example.com>
2020-06-11 13:16:14 +1200

    commit

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+    @app.errorhandler(404)
+def error404(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
